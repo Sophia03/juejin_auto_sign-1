45fbbb54617ca95a26297d5bd0a57f950bc76ad5
chenying <chen.ying@bmsoft.com.cn>
2024-03-22 09:44:19 +0800

    取消沾喜气

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const growth = {
   totalPoint: 0, //总矿石数
   contCount: 0, // 连续签到天数
   totalCount: 0, // 累计签到天数
-  dippedLucky: false, // 是否沾过喜气
-  dipValue: 0, // 幸运值
-  luckyValue: 0, // 总幸运值
   freeCount: 0, //免费抽奖次数
   freeDraw: false, // 是否免费抽奖
   lotteryName: '' // 奖品名称
@@ -20,8 +17,6 @@ function message() {
   当前矿石数 ${growth.totalPoint}
   连续签到天数 ${growth.contCount}
   累计签到天数 ${growth.totalCount}
-  ${growth.dippedLucky ? '今日已经沾过喜气' : `沾喜气 +${growth.dipValue} 幸运值`}
-  当前幸运值 ${growth.luckyValue}
   免费抽奖次数 ${growth.freeCount}
   ${growth.freeDraw ? `恭喜抽中 ${growth.lotteryName}` : '今日已免费抽奖'}
   `.trim()
@@ -35,15 +30,18 @@ async function init() {
     console.log(error)
     return handlePush({ message: '登录失败, 请尝试更新 Cookies', type: 'error' })
   }
+  // 签到
   const isCheckedIn = await request.getTodayStatus()
   if (!isCheckedIn) {
     const { incr_point } = await request.checkIn()
     growth.incrPoint = incr_point
     growth.checkedIn = true
   }
+  // 签到天数
   const { cont_count, sum_count } = await request.getCounts()
   growth.contCount = cont_count
   growth.totalCount = sum_count
+  // 免费抽奖
   const { free_count } = await request.getLotteryConfig()
   growth.freeCount = free_count || 0
   if (free_count > 0) {
@@ -51,15 +49,8 @@ async function init() {
     growth.freeDraw = true
     growth.lotteryName = lottery_name
   }
+  // 当前矿石数
   growth.totalPoint = await request.getCurrentPoint()
-  const { lotteries } = await request.getLotteryHistory()
-  const luckyId = lotteries && lotteries[0] ? lotteries[0]['history_id'] : 0
-  if (lotteries.length > 0) {
-    const { dip_value, total_value, has_dip } = await request.dipLucky(luckyId)
-    growth.dipValue = dip_value || 0
-    growth.luckyValue = total_value
-    growth.dippedLucky = has_dip
-  }
   console.log(growth)
   let msg = message()
   msg = msg.replace(/\n/g, ' \n\n > ').replace(/ +/g, ' ')
